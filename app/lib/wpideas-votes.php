32c33936f3fdeb59712ec57da53a632265bbeae9
mehulkaklotar <mehul.kaklotar@gmail.com>
2014-02-19 11:47:28 +0530

    vote up and down added

---
+++ app/lib/wpideas-votes.php
@@ -0,0 +1,190 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Add data to the wpideas_vote table
+ * 
+ * @global type $post
+ * @global type $rtWpideasVotes
+ * @param type $vote_count
+ * @return type
+ */
+function add_vote( $post, $vote_count = 1 ) {
+	global $rtWpideasVotes;
+	$user = get_current_user_id();
+	$data = array(
+		'post_id' => $post,
+		'user_id' => $user,
+		'vote_count' => $vote_count,
+	);
+	return $rtWpideasVotes->add_vote( $data );
+}
+
+/**
+ * Update vote for the post
+ * 
+ * @global type $post
+ * @global type $rtWpideasVotes
+ * @param type $vote_count
+ * @return type
+ */
+function update_vote( $post , $vote_count) {
+	global $rtWpideasVotes;
+	$user = get_current_user_id();
+	$where = array(
+		'post_id' => $post,
+		'user_id' => $user,
+	);
+	$data = array(
+		'vote_count' => $vote_count,
+	);
+	return $rtWpideasVotes -> update_vote( $data , $where );
+}
+
+/**
+ * Removes the vote entry for the user and post
+ * 
+ * @global type $post
+ * @global type $rtWpideasVotes
+ * @return type
+ */
+function delete_vote() {
+	global $post, $rtWpideasVotes;
+	$user = get_current_user_id();
+	$where = array(
+		'post_id' => $post -> ID,
+		'user_id' => $user,
+	);
+	return $rtWpideasVotes -> delete_vote( $where );
+}
+
+/**
+ * Get votes count for the idea
+ * 
+ * @global type $rtWpideasVotes
+ * @param type $idea
+ * @return type
+ */
+function get_votes_by_idea( $idea ) {
+	global $rtWpideasVotes;
+	return $rtWpideasVotes -> get_votes_by_idea( $idea );
+}
+
+/**
+ * 
+ * @global type $rtWpideasVotes
+ * @param type $post
+ * @return type
+ */
+function get_votes_by_post( $post ) {
+	global $rtWpideasVotes;
+	$vote_count = $rtWpideasVotes -> get_votes_by_post( $post );
+	 if ( $vote_count == null ){
+		 return 0;
+	 }
+	 return $vote_count;
+}
+
+/**
+ * 
+ * @global type $rtWpideasVotes
+ * @return type
+ */
+function get_all_ideas() {
+	global $rtWpideasVotes;
+	return $rtWpideasVotes -> get_all_ideas();
+}
+
+function check_user_voted( $idea ){
+	global $rtWpideasVotes;
+	$user = get_current_user_id();
+	$row = $rtWpideasVotes -> check_user_voted( $idea, $user );
+	if( ! empty( $row[0] ) && $row[0]->vote_count == 1 ){
+		return true;
+	}else if( ! empty( $row[0] ) && $row[0]->vote_count == 0 ){
+		return false;
+	} else if ( empty ( $row[0] ) ){
+		return null;
+	}
+}
+
+
+add_action( 'wp_ajax_vote', 'vote_callback' );
+add_action( 'wp_ajax_nopriv_vote', 'vote_callback' );
+
+function vote_callback() {
+	$response = array();
+	if ( ! is_user_logged_in() ){
+		$response['err'] = 'Please login to vote.';
+	} else {
+		$postid = intval( $_POST['postid'] );
+		$is_voted = check_user_voted( $postid );
+		if ( isset( $is_voted ) && $is_voted ){
+			update_vote( $postid, 0 );
+		} else if( isset( $is_voted ) && ! $is_voted ){
+			update_vote( $postid, 1 );
+		} else if( ! isset ( $is_voted ) ){
+			add_vote( $postid );
+		}
+		$response['vote'] = get_votes_by_post( $postid );
+	}
+
+	echo json_encode( $response );
+	die(); // this is required to return a proper result
+}
+
+
+/**
+ * Shortcode for list of idea
+ * 
+ * @global type $post
+ * @param type $atts
+ * @return string
+ */
+function list_all_idea_shortcode( $atts ) {
+	global $post;
+	$default = array(
+		'type' => 'post',
+		'post_type' => 'idea',
+		//'limit' => 10,
+		'status' => 'publish',
+	);
+	$r = shortcode_atts( $default, $atts );
+	extract( $r );
+
+	if ( empty( $post_type ) )
+		$post_type = $type;
+
+	$post_type_ob = get_post_type_object( $post_type );
+	if ( ! $post_type_ob )
+		return '<div class="warning"><p>No such post type <em>' . $post_type . '</em> found.</p></div>';
+
+	$return = '<h3>' . $post_type_ob->name . '</h3>';
+
+	$args = array(
+		'post_type' => $post_type,
+		'numberposts' => $limit,
+		'post_status' => $status,
+	);
+
+	$posts = get_posts( $args );
+	if ( count( $posts ) ):
+		$return .= '<ul>';
+		foreach ( $posts as $post ): setup_postdata( $post );
+			$return .= '<li>' . get_the_title() . '</li>';
+		endforeach;
+		wp_reset_postdata();
+		$return .= '</ul>';
+	else :
+		$return .= '<p>No ideas found.</p>';
+	endif;
+
+	return $return;
+}
+
+add_shortcode( 'ideas', 'list_all_idea_shortcode' );
--- app/main/RTWPIdeas.php
+++ app/main/RTWPIdeas.php
@@ -10,61 +10,92 @@ class RTWPIdeas {
 
 	private $post_type = 'idea';
 
-	function __construct() {
-		$this->init();
+	/**
+	 * constructor 
+	 */
+	public function __construct() {
+		add_action( 'init', array( $this, 'register_wpidea_post_type' ) );
+		$this->init_attributes();
+		add_action( 'admin_menu', array( $this, 'register_pages' ) );
+		$this->register_taxonomies();
 	}
 
 	/**
-	 * Init all required function like register post-type, etc
+	 * init a globle variable 
 	 */
-	function init() {
-		add_action( 'init', array( $this, 'register_post_type' ), 0 );
+	function init_attributes() {
+		global $rtWPIdeasAttributesModel, $rtWPIdeasAttributes, $rtWpideasVotes;
+		$rtWPIdeasAttributesModel = new RtWPIdeasAttributeTaxonomyModel();
+		$rtWPIdeasAttributes = new RtWPIdeasAttributes();
+		$rtWpideasVotes = new RTWPIdeasVotesModel();
 	}
 
 	/**
-	 * This will return wordpress-idea post-type
-	 * @return string ideas post type
+	 * add attributes page link in menu bar
 	 */
-	function get_post_type() {
-		return $this->post_type;
+	function register_pages() {
+		global $rtWPIdeasAttributes;
+		$attributes = rtwpideas_get_supported_attribute();
+		if ( is_array( $attributes ) && ! empty( $attributes ) ) {
+			foreach ( $attributes as $attribute ) {
+				if ( $attribute !== 'post' ) {
+					add_submenu_page( 'edit.php?post_type=' . $attribute, __( 'Attributes' ), __( 'Attributes' ), 'administrator', $attribute . '-attributes', array( $rtWPIdeasAttributes, 'attributes_page' ) );
+				} else {
+					add_submenu_page( 'edit.php', __( 'Attributes' ), __( 'Attributes' ), 'administrator', $attribute . '-attributes', array( $rtWPIdeasAttributes, 'attributes_page' ) );
+				}
+			}
+		}
 	}
 
 	/**
-	 * This function will register ideas post type in to WordPress
+	 * create a taxonomies
 	 */
-	function register_post_type() {
+	function register_taxonomies() {
+		global $rtWPIdeasAttributesModel, $rtWPIdeasAttributes;
+		$attributes = $rtWPIdeasAttributesModel->get_all_attributes( $this->post_type );
+		if ( is_array( $attributes ) ) {
+			foreach ( $attributes as $attr ) {
+				if ( is_object( $attr ) ) {
+					$rtWPIdeasAttributes->register_taxonomy( $this->post_type, $attr->id );
+				} else {
+					$rtWPIdeasAttributes->register_taxonomy( $this->post_type, 0 );
+				}
+			}
+		}
+	}
+
+	function register_wpidea_post_type() {
 		$labels = array(
-			'name'				 => 'Ideas',
-			'singular_name'		 => 'Idea',
-			'add_new'			 => 'Add New',
-			'add_new_item'		 => 'Add New Idea',
-			'edit_item'			 => 'Edit Idea',
-			'new_item'			 => 'New Idea',
-			'all_items'			 => 'All Ideas',
-			'view_item'			 => 'View Ideas',
-			'search_items'		 => 'Search Ideas',
-			'not_found'			 => 'No Idea found',
-			'not_found_in_trash' => 'No Idea found in Trash',
-			'parent_item_colon'	 => '',
-			'menu_name'			 => 'Ideas'
+			'name' => __( 'Ideas', 'rtCamp' ),
+			'singular_name' => __( 'Idea', 'rtCamp' ),
+			'add_new' => __( 'Add New', 'rtCamp' ),
+			'add_new_item' => __( 'Add New Idea', 'rtCamp' ),
+			'edit_item' => __( 'Edit Idea', 'rtCamp' ),
+			'new_item' => __( 'New Idea', 'rtCamp' ),
+			'all_items' => __( 'All Ideas', 'rtCamp' ),
+			'view_item' => __( 'View Ideas', 'rtCamp' ),
+			'search_items' => __( 'Search Ideas', 'rtCamp' ),
+			'not_found' => __( 'No Idea found', 'rtCamp' ),
+			'not_found_in_trash' => __( 'No Idea found in Trash', 'rtCamp' ),
+			'parent_item_colon' => __( '', 'rtCamp' ),
+			'menu_name' => __( 'Ideas', 'rtCamp' ),
 		);
 
 		$args = array(
-			'labels'			 => $labels,
-			'public'			 => true,
+			'labels' => $labels,
+			'public' => true,
 			'publicly_queryable' => true,
-			'show_ui'			 => true,
-			'show_in_menu'		 => true,
-			'query_var'			 => true,
-			'rewrite'			 => array( 'slug' => $this->post_type, 'with_front' => true ),
-			'has_archive'		 => true,
-			'hierarchical'		 => false,
-			'menu_position'		 => null,
-			'supports'			 => array( 'title', 'editor', 'author', 'excerpt', 'comments' )
+			'show_ui' => true,
+			'show_in_menu' => true,
+			'query_var' => true,
+			'rewrite' => array( 'slug' => $this->post_type, 'with_front' => true ),
+			'has_archive' => true,
+			'hierarchical' => false,
+			'menu_position' => null,
+			'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'revisions', 'excerpt', 'comments', 'page-attributes' )
 		);
 
 		register_post_type( $this->post_type, $args );
 	}
 
 }
-
