c65f02fbb08cc827fd055f80cbcde75b48a00756
Faishal Saiyed <saiyedfaishal@gmail.com>
2014-02-20 14:04:19 +0530

    Fix markup issue

--- app/includes/plugin_template/archive-idea.php
+++ app/includes/plugin_template/archive-idea.php
@@ -7,6 +7,17 @@
  * @since rtPanelChild 2.0
  */
 get_header();
+
+$content_class = apply_filters('rtwiki_content_class', 'large-8 small-12 columns');
+?>
+
+<div id="primary" class="content-area <?php echo $content_class ?>">
+	<div id="content" class="site-content" role="main">
+<?php
 include RTWPIDEAS_PATH . 'app/includes/plugin_template/loop-common.php';
+?>
+
+	</div><!-- #content -->
+</div><
 get_sidebar();
 get_footer();
--- app/includes/plugin_template/loop-common.php
+++ app/includes/plugin_template/loop-common.php
@@ -5,101 +5,113 @@
  * and open the template in the editor.
  */
 //include RTWPIDEAS_PATH . 'app/includes/template-insert-idea.php';
-?>	
-<div id="primary" class="content-area yui-skin-sam <?php apply_filters( 'rtwpideas_content_class', 'large-8 small-12 columns' ); ?>">
+?>
+<div id="primary"
+	 class="content-area yui-skin-sam <?php apply_filters( 'rtwpideas_content_class', 'large-8 small-12 columns' ); ?>">
 	<div id="content" class="site-content" role="main">
 
 		<header class="page-header">
 			<h1 class="page-title"><?php _e( 'Ideas', 'rtCamp' ); ?></h1>
-				<input type="text" placeholder="Enter Idea Here" id="txtNewIdea" name="txtNewIdea" />
-				<input type="button" id="btnSearchSubmit" style="display: none;" />
-			<div id="SuggestionContainer"></div> <!--suggestion window -->
+			<input type="text" placeholder="Enter Idea Here" id="txtNewIdea" name="txtNewIdea"/>
+			<input type="button" id="btnSearchSubmit" style="display: none;"/>
+
+			<div id="SuggestionContainer"></div>
+			<!--suggestion window -->
 			<?php include RTWPIDEAS_PATH . 'app/includes/template-insert-idea.php'; ?>
 		</header>
-		<div id ="res"></div>
-		<?php  if ( have_posts() ) : ?>
+		<div id="res"></div>
+		<?php if ( have_posts() ) : ?>
 			<?php while ( have_posts() ) : the_post(); ?>
-		<?php $str .=  '"'.get_the_title().'",'; ?>
-		                <article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> >
+				<?php $str .= '"' . get_the_title() . '",'; ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> >
 					<div class="rtwpIdeaVoteBadge">
 						<div class="rtwpIdeaVoteCount">
-							<strong id="rtwpIdeaVoteCount-<?php the_ID(); ?>"><?php echo sanitize_text_field( get_votes_by_post( get_the_ID() ) ); ?></strong>
-							<span> votes</span>    
+							<strong
+								id="rtwpIdeaVoteCount-<?php the_ID(); ?>"><?php echo sanitize_text_field( get_votes_by_post( get_the_ID() ) ); ?></strong>
+							<span> votes</span>
 						</div>
 						<div class="rtwpIdeaVoteButton">
-							<button id="btnVote-<?php the_ID(); ?>" class="btnVote" data-id="<?php the_ID(); ?>" >
-	<?php
-		if ( is_user_logged_in() ){
-			$is_voted = check_user_voted( get_the_ID() );
-			if ( isset( $is_voted ) && $is_voted ){
-				echo 'Vote Down';
-			} else if ( isset( $is_voted ) && ! $is_voted ){
-				echo 'Vote Up';
-			} else {
-				echo 'Vote';
-			}
-		} else {
-			echo 'Vote';
-		}
-	?>
+							<button id="btnVote-<?php the_ID(); ?>" class="btnVote" data-id="<?php the_ID(); ?>">
+								<?php
+								if ( is_user_logged_in() ){
+									$is_voted = check_user_voted( get_the_ID() );
+									if ( isset( $is_voted ) && $is_voted ){
+										echo 'Vote Down';
+									} else {
+										if ( isset( $is_voted ) && ! $is_voted ){
+											echo 'Vote Up';
+										} else {
+											echo 'Vote';
+										}
+									}
+								} else {
+									echo 'Vote';
+								}
+								?>
 							</button>
 							<button id="btnLogin" style="display: none;">Login</button>
 						</div>
 					</div>
 					<header class="rtwpIdeaHeader">
-						<h1 class="rtwpIdeaTitle"><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php printf( esc_attr__( 'Permanent Link to %s', 'rtCamp' ), the_title_attribute( 'echo=0' ) ); ?>"><?php the_title(); ?></a></h1>
+						<h1 class="rtwpIdeaTitle"><a href="<?php the_permalink(); ?>" rel="bookmark"
+													 title="<?php printf( esc_attr__( 'Permanent Link to %s', 'rtCamp' ), the_title_attribute( 'echo=0' ) ); ?>"><?php the_title(); ?></a>
+						</h1>
+
 						<div class="rtwpIdeaDescription">
 							<div class="typeset"><?php if ( is_single() ) : the_content();
-		else : the_excerpt();
-		endif; ?>
+								else : the_excerpt();
+								endif; ?>
 							</div>
 						</div>
 						<?php
-		if ( ! is_single() ) {
-			$args = array(
-				'post_parent' => get_the_ID(),
-				'post_type' => 'attachment',
-				'posts_per_page' => -1,
-				'orderby' => 'menu_order',
-				'order' => 'ASC',
-			);
-			$attachments = get_children( $args );
-			if ( ! empty( $attachments ) ){
-				?><ul class="rtwpIdeaAttachments rtwpAttachments"> <?php $i = 0;
-				foreach ( $attachments as $attachment ) {
-					if( $i >= 3 ) break;
-					$image_attributes = wp_get_attachment_image_src( $attachment->ID, 'full' );
-					?>
-					<li class="rtwpAttachment">
-						<a class="rtwpAttachmentLink rtwpAttachmentLink-preview" href="<?php echo esc_url( $attachment->guid ); ?>" title="View <?php echo sanitize_title( $attachment->post_title ); ?>">
-							<figure class="rtwpAttachmentInfo">
-								<span class="rtwpAttachmentThumbnail" style="background-image: url('<?php echo esc_url( wp_get_attachment_thumb_url( $attachment->ID ) ); ?>')">&nbsp;</span>
-								<figcaption class="rtwpAttachmentMeta">
-									<span class="rtwpAttachmentCaption"><?php echo sanitize_title( $attachment->post_title ); ?></span>
-								</figcaption>
-							</figure>
-						</a>
-					</li>
-					<?php $i++;
-				}
-				?></ul><?php
-			}
-		}
-					?>
-						
+						if ( ! is_single() ){
+							$args        = array( 'post_parent' => get_the_ID(), 'post_type' => 'attachment', 'posts_per_page' => - 1, 'orderby' => 'menu_order', 'order' => 'ASC', );
+							$attachments = get_children( $args );
+							if ( ! empty( $attachments ) ){
+								?>
+								<ul class="rtwpIdeaAttachments rtwpAttachments"> <?php $i = 0;
+								foreach ( $attachments as $attachment ) {
+									if ( $i >= 3 ){
+										break;
+									}
+									$image_attributes = wp_get_attachment_image_src( $attachment->ID, 'full' );
+									?>
+									<li class="rtwpAttachment">
+										<a class="rtwpAttachmentLink rtwpAttachmentLink-preview"
+										   href="<?php echo esc_url( $attachment->guid ); ?>"
+										   title="View <?php echo sanitize_title( $attachment->post_title ); ?>">
+											<figure class="rtwpAttachmentInfo">
+												<span class="rtwpAttachmentThumbnail"
+													  style="background-image: url('<?php echo esc_url( wp_get_attachment_thumb_url( $attachment->ID ) ); ?>')">&nbsp;</span>
+												<figcaption class="rtwpAttachmentMeta">
+													<span
+														class="rtwpAttachmentCaption"><?php echo sanitize_title( $attachment->post_title ); ?></span>
+												</figcaption>
+											</figure>
+										</a>
+									</li>
+									<?php $i ++;
+								}
+								?></ul><?php
+							}
+						}
+						?>
+
 						<div class="rtwpIdeaMeta">
-							<a href="<?php the_permalink(); ?>#comments" title="Comments for <?php the_title(); ?>"><?php comments_number( 'No Comments', '1 Comment', '% Comments' ); ?></a> 
-							<span class="uvStyle-separator">&nbsp;·&nbsp;</span> 
+							<a href="<?php the_permalink(); ?>#comments"
+							   title="Comments for <?php the_title(); ?>"><?php comments_number( 'No Comments', '1 Comment', '% Comments' ); ?></a>
+							<span class="uvStyle-separator">&nbsp;·&nbsp;</span>
 							<a href="#" title="Ideas similar to <?php the_title(); ?>">Category</a>
-							<span class="rtwpStyle-separator">&nbsp;·&nbsp;</span>  
-							<a href="<?php echo get_the_author_link(); ?>" data-iframe-target="_blank" title="Author of <?php the_title(); ?>"><?php echo get_the_author(); ?> →</a>
+							<span class="rtwpStyle-separator">&nbsp;·&nbsp;</span>
+							<a href="<?php echo get_the_author_link(); ?>" data-iframe-target="_blank"
+							   title="Author of <?php the_title(); ?>"><?php echo get_the_author(); ?> →</a>
 						</div>
 					</header>
-		                </article>
+				</article>
 				<?php
-		if ( is_single() ) :
-			comments_template();
-		endif;
+				if ( is_single() ) :
+					comments_template();
+				endif;
 			endwhile;
 			?>
 			<div class="navigation">
@@ -109,8 +121,9 @@
 
 
 		<?php else : ?>
-	<?php get_template_part( 'content', 'none' ); ?>
-<?php endif; ?>
-<?php include RTWPIDEAS_PATH . 'app/includes/search-suggestion.php'; ?>
-	</div><!-- #content -->
+			<?php get_template_part( 'content', 'none' ); ?>
+		<?php endif; ?>
+		<?php include RTWPIDEAS_PATH . 'app/includes/search-suggestion.php'; ?>
+	</div>
+	<!-- #content -->
 </div><!-- #primary -->
