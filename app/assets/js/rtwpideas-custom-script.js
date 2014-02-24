ef9a075887541d571c255e88eec3b2f7eaf64f1f
mehulkaklotar <mehul.kaklotar@gmail.com>
2014-02-24 14:42:31 +0530

    highlight the search text

--- app/assets/js/rtwpideas-custom-script.js
+++ app/assets/js/rtwpideas-custom-script.js
@@ -18,7 +18,7 @@ jQuery(document).ready(function($) {
 			if (json.vote) {
 				$('#rtwpIdeaVoteCount-' + data['postid']).html(json.vote);
 				$('#btnVote-' + data['postid']).removeAttr('disabled');
-				$('#btnVote-' + data['postid']).html(json.btnLabel);
+				$('#btnVote-' + data['postid']).attr('value', json.btnLabel);
 			} else {
 				alert(json.err);
 				//$('#btnVote-' + data['postid']).hide();
@@ -26,29 +26,87 @@ jQuery(document).ready(function($) {
 			}
 		});
 	});
-	
-	jQuery('#insertIdeaForm').submit(function(){
-		alert("");
-		var data =  jQuery(this).serialize();
-
-		$.post(rt_wpideas_ajax_url, data, function(response) {
-
-			alert(response);
-		});
-		
-	});
 
 	jQuery('#txtSearchIdea').keyup(function() {
+
 		var data = {
 			action: 'search',
 			searchtext: $(this).val(),
 		};
-		
+
 		jQuery.post(rt_wpideas_ajax_url, data, function(response) {
 			$('#res').html(response);
+			// pull in the new value
+			var searchTerm = data['searchtext'];
+			// remove any old highlighted terms
+			$('#res').removeHighlight();
+			// disable highlighting if empty
+			if (searchTerm) {
+				// highlight the new term
+				$('#res').highlight(searchTerm);
+			}
 		});
+
 	});
 
-	//jQuery("#primaryPostForm").validate();
-
 });
+
+jQuery.fn.highlight = function(pat) {
+	function innerHighlight(node, pat) {
+		var skip = 0;
+		if (node.nodeType == 3) {
+			var pos = node.data.toUpperCase().indexOf(pat);
+			if (pos >= 0) {
+				var spannode = document.createElement('span');
+				spannode.className = 'highlight';
+				var middlebit = node.splitText(pos);
+				var endbit = middlebit.splitText(pat.length);
+				var middleclone = middlebit.cloneNode(true);
+				spannode.appendChild(middleclone);
+				middlebit.parentNode.replaceChild(spannode, middlebit);
+				skip = 1;
+			}
+		}
+		else if (node.nodeType == 1 && node.childNodes && !/(script|style)/i.test(node.tagName)) {
+			for (var i = 0; i < node.childNodes.length; ++i) {
+				i += innerHighlight(node.childNodes[i], pat);
+			}
+		}
+		return skip;
+	}
+	return this.each(function() {
+		innerHighlight(this, pat.toUpperCase());
+	});
+};
+
+jQuery.fn.removeHighlight = function() {
+	function newNormalize(node) {
+		for (var i = 0, children = node.childNodes, nodeCount = children.length; i < nodeCount; i++) {
+			var child = children[i];
+			if (child.nodeType == 1) {
+				newNormalize(child);
+				continue;
+			}
+			if (child.nodeType != 3) {
+				continue;
+			}
+			var next = child.nextSibling;
+			if (next == null || next.nodeType != 3) {
+				continue;
+			}
+			var combined_text = child.nodeValue + next.nodeValue;
+			new_node = node.ownerDocument.createTextNode(combined_text);
+			node.insertBefore(new_node, child);
+			node.removeChild(child);
+			node.removeChild(next);
+			i--;
+			nodeCount--;
+		}
+	}
+
+	return this.find("span.highlight").each(function() {
+		var thisParent = this.parentNode;
+		thisParent.replaceChild(this.firstChild, this);
+		newNormalize(thisParent);
+	}).end();
+};
