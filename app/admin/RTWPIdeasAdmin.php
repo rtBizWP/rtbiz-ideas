a334c8f1df36ef9d6bb266ebcc76386716d24a17
mehulkaklotar <mehul.kaklotar@gmail.com>
2014-02-27 16:25:37 +0530

    Woocommerce panel code removed

--- app/admin/RTWPIdeasAdmin.php
+++ app/admin/RTWPIdeasAdmin.php
@@ -19,15 +19,15 @@ if ( ! class_exists( 'RTWPIdeasAdmin' ) ) {
 		 */
 		public function __construct() {
 			add_action( 'init', array( $this, 'register_wpidea_post_type' ) );
-			add_action( 'admin_init', array( $this, 'wpideas_custom_post_status' ) );
+			add_action( 'init', array( $this, 'wpideas_custom_post_status' ) );
 			add_action( 'wp_before_admin_bar_render', array( $this, 'wpideas_append_post_status_list' ), 11 );
+			if ( get_post_type() == RT_WPIDEAS_SLUG ) {
+				add_filter( 'manage_edit_idea_columns', 'wpideas_ideas_table_head' );
+				add_action( 'manage_idea_posts_custom_column', 'wpideas_ideas_table_columns', 10, 2 );
+			}
 			$this -> init_attributes();
 			//add_action( 'admin_menu', array( $this, 'register_pages' ) );
 			//$this -> register_taxonomies();
-			//add_action( 'woocommerce_product_write_panel_tabs', array( $this, 'custom_tab_wpideas_tab' ), 11 );
-			//add_action( 'woocommerce_product_write_panels', array( $this, 'custom_tab_wpideas_content' ) );
-			//add_action( 'woocommerce_process_product_meta', array( $this, 'process_product_meta_custom_tab_wpideas' ) );
-			//add_filter( 'woocommerce_product_tabs', array( $this, 'woocommerce_product_custom_tab_wpideas' ), 98 );
 		}
 
 		/**
@@ -152,7 +152,8 @@ if ( ! class_exists( 'RTWPIdeasAdmin' ) ) {
 		}
 
 		function wpideas_append_post_status_list() {
-			if ( get_post_type() == RT_WPIDEAS_SLUG && $_GET[ 'action' ] == 'edit' ) {
+			global $pagenow;
+			if ( get_post_type() == RT_WPIDEAS_SLUG && ( $pagenow == 'edit.php' || ( isset( $_GET[ 'action' ] ) && $_GET[ 'action' ] ) == 'edit' ) ) {
 				global $post;
 				if ( ! isset( $post ) ) {
 					return;
@@ -202,82 +203,16 @@ if ( ! class_exists( 'RTWPIdeasAdmin' ) ) {
 			}
 		}
 
-		function custom_tab_wpideas_tab() {
-			?>
-			<li class="rtp-swt-custom-tabs custom-tab-wpideas"><a href="#custom-tab-wpideas"><?php _e( 'Ideas', 'wp-ideas' ); ?></a></li>
-			<?php
+		function wpideas_ideas_table_head( $defaults ) {
+			$defaults[ 'wpideas_votes' ] = _x( 'Votes', 'wp-ideas');
+			return $defaults;
 		}
 
-		function custom_tab_wpideas_content() {
-			global $post;
-
-			$custom_tab_options = array(
-				'title' => get_post_meta( $post -> ID, '_wpideas_tab_title', true ),
-				'content' => get_post_meta( $post -> ID, '_wpideas_tab_content', true ),
-			);
-			?>
-			<div id="custom-tab-wpideas" class="panel woocommerce_options_panel">
-				<div class="options_group">
-					<?php woocommerce_wp_checkbox( array( 'id' => '_wpideas_tab_enabled', 'label' => __( 'Enable Ideas Tab?', 'wp-ideas' ) ) ); ?>
-
-					<p class="form-field">
-						<label><?php _e( 'Tab Title:', 'wp-ideas' ); ?></label>
-						<input type="text" size="5" name="_wpideas_tab_title" value="<?php echo ( isset( $custom_tab_options[ 'title' ] ) && ! empty( $custom_tab_options[ 'title' ] ) ) ? $custom_tab_options[ 'title' ] : __( 'Ideas' ); ?>" placeholder="<?php _e( 'Ideas' ); ?>" />
-					</p>
-
-					<table class="form-table">
-						<tr>
-							<td>
-								<?php
-								$settings = array(
-									'text_area_name' => '_wpideas_tab_content',
-									'quicktags' => true,
-									'tinymce' => true,
-									'media_butons' => false,
-									'textarea_rows' => 50,
-									'editor_class' => 'contra',
-									'editor_css' => '<style>#wp-_tour_itinerary_tab_content-editor-container .wp-editor-area{height:250px; width:100%;} #wp-_tour_itinerary_tab_content-editor-container .quicktags-toolbar input {width:auto;}</style>'
-								);
-
-								$id = '_wpideas_tab_content';
-
-								wp_editor( $custom_tab_options[ 'content' ], $id, $settings );
-								?>
-							</td>
-						</tr>
-					</table>
-				</div>
-			</div>
-			<?php
-		}
-
-		function process_product_meta_custom_tab_wpideas( $post_id ) {
-			update_post_meta( $post_id, '_wpideas_tab_enabled', ( isset( $_POST[ '_wpideas_tab_enabled' ] ) && $_POST[ '_wpideas_tab_enabled' ] ) ? 'yes' : 'no'  );
-			update_post_meta( $post_id, '_wpideas_tab_title', $_POST[ '_wpideas_tab_title' ] );
-			update_post_meta( $post_id, '_wpideas_tab_content', $_POST[ '_wpideas_tab_content' ] );
-		}
-
-		function woocommerce_product_custom_tab_wpideas( $tabs ) {
-			global $post;
-
-			$enabled = get_post_meta( $post -> ID, '_wpideas_tab_enabled', true );
-
-			if ( $enabled != 'yes' )
-				return $tabs;
-
-			$custom_tab_options[ 'custom_tab_wpideas' ] = array(
-				'title' => get_post_meta( $post -> ID, '_wpideas_tab_title', true ),
-				'priority' => 50,
-				'callback' => array( $this, 'woocommerce_product_custom_panel_wpideas' ),
-			);
-
-			$tabs = array_merge( $tabs, $custom_tab_options );
-			return $tabs;
-		}
-
-		function woocommerce_product_custom_panel_wpideas() {
-			global $post;
-			echo '<p>' . wpautop( get_post_meta( $post -> ID, '_wpideas_tab_content', true ) ) . '</p>';
+		function wpideas_ideas_table_content( $column_name, $idea_id ) {
+			if ( $column_name == 'wpideas_votes' ) {
+				$votes = get_post_meta( $idea_id, '_rt_wpideas_meta_votes', true );
+				echo $votes;
+			}
 		}
 
 	}
