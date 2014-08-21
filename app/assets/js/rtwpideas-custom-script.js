b4bcc9efc68ee95b4d4077b7c663fb1aba2e5f42
Udit Desai <desaiuditd@gmail.com>
2014-08-21 14:24:19 +0530

    Revert "jquery undefined have_more_posts issue fix"
    This reverts commit 272e557a0c772efc8ed1ec152b8811da38ab71eb.

--- app/assets/js/rtwpideas-custom-script.js
+++ app/assets/js/rtwpideas-custom-script.js
@@ -79,13 +79,11 @@ jQuery(document).ready(function ($) {
                 if (response.have_posts) {//if have posts:
                     var $newElems = $(response['html'].replace(/(\r\n|\n|\r)/gm, ''));
                     $('#wpidea-content').append($newElems);
-                    
-                    if (!response.have_more_posts) {
-                     $('#ideaLoadMore').hide();
-                    }
                 } 
                 
-                
+                if (!response.have_more_posts) {
+                     $('#ideaLoadMore').hide();
+                }
             }
         });
     });
