463857d6b5496a3a1e0e87cb40c5ec408f063ed1
Kishore Chandra Sahoo <babunsahoo@gmail.com>
2014-09-01 17:25:09 +0530

    chaning hook from save post to draft to publish

--- app/admin/RTWPIdeasAutoProductSynchronization.php
+++ app/admin/RTWPIdeasAutoProductSynchronization.php
@@ -28,7 +28,7 @@ if( !class_exists( 'RTWPIdeasAutoProductSynchronization' ) ){
 		
 		function hooks() {
 			if ( get_option( 'wpideas_auto_product_synchronizationenabled' ) == 1 ){
-				add_action( 'save_post', array( $this, 'insert_products' ) );
+				add_action( 'draft_to_publish', array( $this, 'insert_products' ) );
 				add_action( 'wp_untrash_post', array( $this, 'insert_products' ) );
 				add_action( 'delete_post', array( $this, 'delete_products' ) );
 				add_action( 'trashed_post', array( $this, 'delete_products' ) );
@@ -48,25 +48,28 @@ if( !class_exists( 'RTWPIdeasAutoProductSynchronization' ) ){
 		 * @access public
 		 * @return void
 		 */
-		public function insert_products() {
-			$args = array( 'posts_per_page' => -1, 'post_type' => 'product' ); 
-			$products_array = get_posts( $args ); // Get Woo Commerce post object
-			$product_names = wp_list_pluck( $products_array, 'post_title' ); // Get Woo Commerce post_title
-			$product_ids = wp_list_pluck( $products_array, 'ID' ); // Get Woo Commerce Post ID
-			
-			$taxonomies = array(
-				'product' => $product_names
-			);
-	
-	
-			foreach ( $taxonomies as $taxonomy => $terms ) {
-				foreach ( $terms as $term ) {
-					if ( ! get_term_by( 'slug', sanitize_title( $term ), $taxonomy ) && $taxonomy == "product"){
-						wp_insert_term( $term, $taxonomy );
-					}
-				}
-			}
-			
+		public function insert_products( $post ) {
+		
+		  $args = array( 'posts_per_page' => -1, 'post_type' => 'product' );
+		  $products_array = get_posts( $args ); // Get Woo Commerce post object
+		  $product_names = wp_list_pluck( $products_array, 'post_title' ); // Get Woo Commerce post_title
+		  $product_ids = wp_list_pluck( $products_array, 'ID' ); // Get Woo Commerce Post ID
+		
+		  $taxonomies = array(
+		    'product' => $product_names
+		  );
+		
+		
+		  foreach ( $taxonomies as $taxonomy => $terms ) {
+		    foreach ( $terms as $term ) {
+		      if ( ! get_term_by( 'slug', sanitize_title( $term ), $taxonomy ) && $taxonomy == "product"){
+		      	$term = wp_insert_term( $term, $taxonomy );
+			  	$term_id = $term["term_id"];
+			  	Rt_Wp_Ideas_Taxonomy_Metadata\add_term_meta($term_id, "_product_id", "product_id", true); // todo: need to fetch product_id
+		      }
+		    }
+		  }
+		
 		}
 		
 		/**
