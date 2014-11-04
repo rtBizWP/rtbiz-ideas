ff316be399fcbae7355bee3e90019fa0dc18f41b
Utkarsh Patel <utkarsh.patel@rtcamp.com>
2014-11-04 16:18:41 +0530

    label fix

--- app/helper/wpideas-common.php
+++ app/helper/wpideas-common.php
@@ -326,7 +326,7 @@ function list_post_ideas( $atts ) {
 		?>
 		</div>
 			<?php
-			echo '<a id="btnOpenThickbox" href="#TB_inline?width=600&height=550&inlineId=my-content-id" class="thickbox"> Suggest Idea for this product</a>';
+			echo '<a id="btnOpenThickbox" href="#TB_inline?width=600&height=550&inlineId=my-content-id" class="thickbox"> Suggest Idea for this post</a>';
 	} else {
 		echo '<br/><a id="btnOpenThickbox" href="/wp-login.php">Login to Suggest Idea</a>';
 	}
