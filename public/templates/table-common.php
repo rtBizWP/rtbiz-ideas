785b54148c3556362499c816db3a97eb3bb329d3
Utkarsh Patel <iamutkarsh@live.com>
2015-09-14 13:13:12 +0530

    Table css change and remove votedown/up from my ideas

--- public/templates/table-common.php
+++ public/templates/table-common.php
@@ -1,6 +1,6 @@
 <tr>
 
-	<td class="rtbiz-ideas-title">
+	<td>
 		<strong>
 			<a href="<?php the_permalink(); ?>" rel="bookmark"
 			   title="<?php printf( esc_attr__( '%s', RTBIZ_IDEAS_TEXT_DOMAIN ), the_title_attribute( 'echo=0' ) ); ?>"> <?php the_title() ?></a>
@@ -14,12 +14,12 @@
 		<a href="<?php echo get_author_posts_url( $author->ID ); ?>"
 		   title="Author of <?php the_title(); ?>"><?php the_author(); ?> </a></td>
 	<?php } ?>
-	<td>
+	<td class="column-votes">
 		<strong id="rtwpIdeaVoteCount-<?php the_ID(); ?>"><?php echo sanitize_text_field( rtbiz_ideas_get_votes_by_idea( get_the_ID() ) ); ?></strong>
 	</td>
 	<td>
 		<div class="rtbiz-idea-action"><?php
-			rtbiz_ideas_get_vote_action( true );
+//			rtbiz_ideas_get_vote_action( true );
 			rtbiz_ideas_get_subscribe_action();?>
 		</div>
 	</td>
